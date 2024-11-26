0e389d9f57c80cfd122aa313ec7c02e29789f4a7
Mads Fich Engelbreth <mfe@dnmh.dk>
2024-11-27 00:05:13 +0100

    less swagger

--- Samples/Program.cs
+++ Samples/Program.cs
@@ -4,19 +4,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
+//builder.Services.AddSwaggerGen();
 
 // Adds the default authentication scheme named "ApiKey". This is activated on a controller by using the [Authorize] attribute.
 builder.Services.AddAuthentication("ApiKey")
-    .AddApiKeyAuthentication(sp => builder.Configuration.GetRequiredSection("MyApiKey").Value!)
-    .AddSwaggerAuthorization("Please add a valid api key (the valid api key is found in appsettings.json 🤫)");
+    .AddApiKeyAuthentication(sp => builder.Configuration.GetRequiredSection("MyApiKey").Value!);
+    //.AddSwaggerAuthorization("Please add a valid api key (the valid api key is found in appsettings.json 🤫)");
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
--- Source/AuthenticationHandler/ApiKeyAuthenticationBuilder.cs
+++ Source/AuthenticationHandler/ApiKeyAuthenticationBuilder.cs
@@ -2,11 +2,11 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Dnmh.Security.ApiKeyAuthentication.AuthenticationHandler;
 
 /// <summary>
+/// TODO: fix summary
 /// Specialized <see cref="AuthenticationBuilder"/> which enables Swagger authorization to be added.
 /// </summary>
 public class ApiKeyAuthenticationBuilder : AuthenticationBuilder
@@ -21,24 +21,5 @@ public class ApiKeyAuthenticationBuilder : AuthenticationBuilder
     public ApiKeyAuthenticationBuilder(string authenticationScheme, IServiceCollection services) : base(services)
     {
         _authenticationScheme = authenticationScheme;
-    }
-
-    /// <summary>
-    /// Adds swagger authorization to the swagger generator.
-    /// </summary>
-    /// <param name="description">The description for this authentication scheme</param>
-    public ApiKeyAuthenticationBuilder AddSwaggerAuthorization(string description = "Please add a valid api key")
-    {
-        Services.Configure<ApiKeySchemeSwaggerOptions>(_authenticationScheme, options =>
-        {
-            options.AuthenticationScheme = _authenticationScheme;
-            options.SwaggerDescription = description;
-        });
-        Services.AddSingleton<IPostConfigureOptions<SwaggerGenOptions>, PostConfigureSwaggerAuthorization>(sp =>
-            new PostConfigureSwaggerAuthorization(_authenticationScheme,
-                sp.GetRequiredService<IOptionsMonitor<ApiKeySchemeSwaggerOptions>>(),
-                sp.GetRequiredService<IOptionsMonitor<ApiKeyAuthenticationOptions>>()));
-
-        return this;
     }
 }
