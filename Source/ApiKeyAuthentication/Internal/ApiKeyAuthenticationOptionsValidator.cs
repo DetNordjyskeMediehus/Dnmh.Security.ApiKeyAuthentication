d41a7279e566a64d9158ba810d5b2a55422fcfad
Pelle Stenild Coltau <psc@dnmh.dk>
2025-01-31 13:08:38 +0100

    Removed FluentValidation

--- Source/ApiKeyAuthentication/Internal/ApiKeyAuthenticationOptionsValidator.cs
+++ Source/ApiKeyAuthentication/Internal/ApiKeyAuthenticationOptionsValidator.cs
@@ -1,27 +1,40 @@
-﻿using FluentValidation;
-
-namespace DNMH.Security.ApiKeyAuthentication.Internal;
+﻿namespace DNMH.Security.ApiKeyAuthentication.Internal;
 
 /// <summary>
 /// Validator for <see cref="ApiKeyAuthenticationOptions"/>
 /// </summary>
-internal class ApiKeyAuthenticationOptionsValidator : AbstractValidator<ApiKeyAuthenticationOptions>
+internal class ApiKeyAuthenticationOptionsValidator
 {
-    public ApiKeyAuthenticationOptionsValidator()
+    public void ValidateAndThrow(ApiKeyAuthenticationOptions options)
     {
-        RuleFor(x => x.AllowApiKeyInQuery).Must(x => x).When(x => !x.AllowApiKeyInRequestHeader)
-            .WithMessage($"Both {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInQuery)} and {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInRequestHeader)} cannot be false");
-        RuleFor(x => x.AllowApiKeyInRequestHeader).Must(x => x).When(x => !x.AllowApiKeyInQuery)
-            .WithMessage($"Both {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInQuery)} and {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInRequestHeader)} cannot be false");
-        RuleFor(x => x.AllowApiKeyInRequestHeader).Must(x => x).When(x => x.UseAuthorizationHeaderKey)
-            .WithMessage($"Setting {nameof(ApiKeyAuthenticationOptions.UseAuthorizationHeaderKey)} to true requires {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInRequestHeader)} to also be true");
-        RuleFor(x => x.AllowApiKeyInRequestHeader).Must(x => x).DependentRules(() => RuleFor(x => x.UseAuthorizationHeaderKey).Must(x => x)).When(x => x.UseSchemeNameInAuthorizationHeader)
-            .WithMessage($"Setting {nameof(ApiKeyAuthenticationOptions.UseSchemeNameInAuthorizationHeader)} to true requires {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInRequestHeader)} and {nameof(ApiKeyAuthenticationOptions.UseAuthorizationHeaderKey)} to also be true");
-        RuleFor(x => x.HeaderKeys).NotNull().When(x => x.AllowApiKeyInRequestHeader)
-            .WithMessage($"{nameof(ApiKeyAuthenticationOptions.HeaderKeys)} must be non-empty when {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInRequestHeader)} is true");
-        RuleFor(x => x.QueryKeys).NotNull().When(x => x.AllowApiKeyInQuery)
-            .WithMessage($"{nameof(ApiKeyAuthenticationOptions.QueryKeys)} must be non-empty when {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInQuery)} is true");
-        RuleFor(x => x.AuthorizationSchemeInHeader).NotNull().NotEmpty().When(x => x.UseAuthorizationHeaderKey && !x.UseSchemeNameInAuthorizationHeader)
-            .WithMessage($"{nameof(ApiKeyAuthenticationOptions.AuthorizationSchemeInHeader)} must be non-empty when {nameof(ApiKeyAuthenticationOptions.UseAuthorizationHeaderKey)} is true and {nameof(ApiKeyAuthenticationOptions.UseSchemeNameInAuthorizationHeader)} is false");
+        if (!options.AllowApiKeyInQuery && !options.AllowApiKeyInRequestHeader)
+        {
+            throw new ArgumentException($"Both {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInQuery)} and {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInRequestHeader)} cannot be false");
+        }
+
+        if (options.UseAuthorizationHeaderKey && !options.AllowApiKeyInRequestHeader)
+        {
+            throw new ArgumentException($"Setting {nameof(ApiKeyAuthenticationOptions.UseAuthorizationHeaderKey)} to true requires {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInRequestHeader)} to also be true");
+        }
+
+        if (options.UseSchemeNameInAuthorizationHeader && (!options.AllowApiKeyInRequestHeader || !options.UseAuthorizationHeaderKey))
+        {
+            throw new ArgumentException($"Setting {nameof(ApiKeyAuthenticationOptions.UseSchemeNameInAuthorizationHeader)} to true requires {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInRequestHeader)} and {nameof(ApiKeyAuthenticationOptions.UseAuthorizationHeaderKey)} to also be true");
+        }
+
+        if (options.AllowApiKeyInRequestHeader && options.HeaderKeys == null)
+        {
+            throw new ArgumentException($"{nameof(ApiKeyAuthenticationOptions.HeaderKeys)} must be non-empty when {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInRequestHeader)} is true");
+        }
+
+        if (options.AllowApiKeyInQuery && options.QueryKeys == null)
+        {
+            throw new ArgumentException($"{nameof(ApiKeyAuthenticationOptions.QueryKeys)} must be non-empty when {nameof(ApiKeyAuthenticationOptions.AllowApiKeyInQuery)} is true");
+        }
+
+        if (options.UseAuthorizationHeaderKey && !options.UseSchemeNameInAuthorizationHeader && string.IsNullOrEmpty(options.AuthorizationSchemeInHeader))
+        {
+            throw new ArgumentException($"{nameof(ApiKeyAuthenticationOptions.AuthorizationSchemeInHeader)} must be non-empty when {nameof(ApiKeyAuthenticationOptions.UseAuthorizationHeaderKey)} is true and {nameof(ApiKeyAuthenticationOptions.UseSchemeNameInAuthorizationHeader)} is false");
+        }
     }
 }
